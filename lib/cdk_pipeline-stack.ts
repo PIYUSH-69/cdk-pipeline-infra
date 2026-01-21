35d37c5b45de29cdc59762fcfc25cc212b5187dd
PIYUSH-69 <piyush.mangesh-dalvi@capgemini.com>
2026-01-21 16:52:59 +0530

    LAMBDA CODE

--- lib/cdk_pipeline-stack.ts
+++ lib/cdk_pipeline-stack.ts
@@ -1,3 +1,4 @@
+import { aws_lambda } from 'aws-cdk-lib';
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -12,5 +13,21 @@ export class CdkPipelineStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'CdkPipelineQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+
+    
+const lambda = new aws_lambda.Function(this, "demoLambda", {
+  functionName: "demo_lambda_func",
+  handler: "index.lambda_handler",
+  runtime: aws_lambda.Runtime.PYTHON_3_10,
+  code: aws_lambda.Code.fromInline(`
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": "Hello from inline Lambda!"
+    }
+`)
+});
+
   }
 }
